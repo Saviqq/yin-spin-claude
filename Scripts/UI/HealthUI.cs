2b8c555ca644fc3e59ce556dc02dd2ce9e67f975
Saviqq <patriksavka@gmail.com>
2026-03-07 16:40:41 +0100

    script changes

--- Scripts/UI/HealthUI.cs
+++ Scripts/UI/HealthUI.cs
@@ -5,17 +5,19 @@ public class HealthUI : MonoBehaviour
 {
     [SerializeField] private IntegerValue health;
 
-    private Label[] heartLabels;
+    private VisualElement[] heartElements;
 
     void OnEnable()
     {
         var root = GetComponent<UIDocument>().rootVisualElement;
 
-        heartLabels = new Label[]
+        heartElements = new VisualElement[]
         {
-            root.Q<Label>("heart-0"),
-            root.Q<Label>("heart-1"),
-            root.Q<Label>("heart-2"),
+            root.Q<VisualElement>("heart-0"),
+            root.Q<VisualElement>("heart-1"),
+            root.Q<VisualElement>("heart-2"),
+            root.Q<VisualElement>("heart-3"),
+            root.Q<VisualElement>("heart-4"),
         };
 
         health.OnChange += OnHealthChange;
@@ -29,10 +31,11 @@ public class HealthUI : MonoBehaviour
 
     private void OnHealthChange(int currentHealth)
     {
-        for (int i = 0; i < heartLabels.Length; i++)
+        for (int i = 0; i < heartElements.Length; i++)
         {
             bool filled = i < currentHealth;
-            heartLabels[i].EnableInClassList("heart--lost", !filled);
+            heartElements[i].EnableInClassList("heart--filled", filled);
+            heartElements[i].EnableInClassList("heart--lost", !filled);
         }
     }
 }
