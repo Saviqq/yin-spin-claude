9aa6c7c71289f3c06679ba20446d872a9ccb156f
Saviqq <patriksavka@gmail.com>
2026-03-08 16:55:53 +0100

    script changes

--- Scripts/PlayerMovement.cs
+++ Scripts/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D)), RequireComponent(typeof(CircleCollider2D))]
@@ -9,20 +10,100 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private FloatValue halfWidthPlayArea;
     [SerializeField] private FloatValue halfHeightPlayArea;
 
+    [Header("Scale Effect")]
+    [SerializeField] private float scaleDuration = 5f;
+
+    [Header("Events")]
+    [SerializeField] private GameEvent gameStartEvent;
+    [SerializeField] private GameEvent expandPlayerEvent;
+    [SerializeField] private GameEvent shrinkPlayerEvent;
+
     private Rigidbody2D rb;
+    private CircleCollider2D col;
     private float playerRadius;
+
+    private float baseMoveSpeed;
+    private float baseRotateSpeed;
+    private float baseScale;
+    private float baseRadius;
+
+    private Coroutine activeScaleCoroutine;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        playerRadius = GetComponent<CircleCollider2D>().bounds.extents.x;
+        col = GetComponent<CircleCollider2D>();
+        playerRadius = col.bounds.extents.x;
+
+        baseMoveSpeed = moveSpeed;
+        baseRotateSpeed = rotateSpeed;
+        baseScale = transform.localScale.x;
+        baseRadius = playerRadius;
+    }
+
+    void OnEnable()
+    {
+        gameStartEvent.OnRaised += OnGameStart;
+        expandPlayerEvent.OnRaised += OnExpand;
+        shrinkPlayerEvent.OnRaised += OnShrink;
+    }
+
+    void OnDisable()
+    {
+        gameStartEvent.OnRaised -= OnGameStart;
+        expandPlayerEvent.OnRaised -= OnExpand;
+        shrinkPlayerEvent.OnRaised -= OnShrink;
+    }
+
+    private void OnExpand() => StartScaleEffect(1.25f, 0.75f);
+    private void OnShrink() => StartScaleEffect(0.75f, 1.25f);
+
+    private void StartScaleEffect(float scaleMult, float speedMult)
+    {
+        if (activeScaleCoroutine != null)
+            StopCoroutine(activeScaleCoroutine);
+
+        activeScaleCoroutine = StartCoroutine(ScaleEffect(scaleMult, speedMult));
+    }
+
+    private IEnumerator ScaleEffect(float scaleMult, float speedMult)
+    {
+        ApplyScaleValues(baseScale * scaleMult, baseMoveSpeed * speedMult, baseRotateSpeed * speedMult);
+
+        yield return new WaitForSeconds(scaleDuration);
+
+        ResetScaleEffect();
+    }
+
+    private void ApplyScaleValues(float scale, float move, float rotate)
+    {
+        transform.localScale = Vector3.one * scale;
+        playerRadius = baseRadius * scale;
+        moveSpeed = move;
+        rotateSpeed = rotate;
+    }
+
+    private void ResetScaleEffect()
+    {
+        ApplyScaleValues(baseScale, baseMoveSpeed, baseRotateSpeed);
+        activeScaleCoroutine = null;
+    }
+
+    private void OnGameStart()
+    {
+        if (activeScaleCoroutine != null)
+        {
+            StopCoroutine(activeScaleCoroutine);
+            activeScaleCoroutine = null;
+        }
+        ResetScaleEffect();
     }
 
     private void HandleRotation()
     {
         float input = 0f;
-        if (Input.GetKey(KeyCode.LeftArrow)) input = 1f; // counter-clockwise
-        else if (Input.GetKey(KeyCode.RightArrow)) input = -1f; // clockwise
+        if (Input.GetKey(KeyCode.LeftArrow)) input = 1f;
+        else if (Input.GetKey(KeyCode.RightArrow)) input = -1f;
 
         rb.MoveRotation(rb.rotation + input * rotateSpeed * Time.fixedDeltaTime);
     }
@@ -47,7 +128,6 @@ public class PlayerMovement : MonoBehaviour
         newPos.y = Mathf.Clamp(newPos.y, -yBound, yBound);
 
         rb.MovePosition(newPos);
-
     }
 
     public void Handle()
